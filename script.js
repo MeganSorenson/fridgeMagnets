ff426714688c74fa407334cd4d0748518b1806c8
MeganSorenson <85324120+MeganSorenson@users.noreply.github.com>
2022-08-27 11:00:07 -0700

    draggable magnets working

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function processText() {
     // takes in a string of lots of words and splits them by spaces
     // returns a list of strings, each being a word
     let text = document.getElementById("input-text").value;
-    var textArray = text.split(" ");
+    let textArray = text.split(" ");
 
     let words = ['.', ',', '!', '?']
 
@@ -11,5 +11,52 @@ function processText() {
         words.push(randElement)
     }
 
-    console.log(words);
+    // call function to create magnets on screen
+    createMagnetSpace(words)
+}
+
+let width = window.innerWidth;
+let height = window.innerHeight / 2;
+let stage = new Konva.Stage({
+    container: 'container',
+    width: width,
+    height: height,
+    draggable: false
+});
+
+let rectangleLayer = new Konva.Layer();
+
+function createMagnetSpace(words) {
+    // creates movable magnets on a space of the words processed from processText()
+    // takes in an array of strs, each being a word
+    // uses Konva library to make magnets dragable
+    for (let word of words) {
+        console.log(word)
+        let rectangle = new Konva.Group({
+            x: 25,
+            y: 25,
+            width: 130,
+            height: 25,
+            rotation: 0,
+            draggable: true,
+        });
+
+        rectangle.add(new Konva.Rect({
+            width: 130,
+            height: 25,
+            fill: 'lightblue'
+        }));
+
+        rectangle.add(new Konva.Text({
+            text: word,
+            fontSize: 18,
+            fontFamily: 'Calibri',
+            fill: '#000',
+            width: 130,
+            padding: 5,
+            align: 'center'
+        }));
+        rectangleLayer.add(rectangle);
+        stage.add(rectangleLayer);
+    }
 }
